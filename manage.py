8739eedbdb2fc6135da1f2aa464aca4f9445b402
Berezovskiy Denis <berezovskiy.denis@gmail.com>
2018-06-19 14:05:17 +0600

    Add async function to call KFM

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
 from app import create_app, db
 from app.models import User, Terrorist, Org
 
+import threading
+import os
+
 
 app = create_app()
 
@@ -8,3 +11,16 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, Terrorist=Terrorist, Org=Org)
+
+
+def call_async_kfm(app):
+    with app.app_context():
+        Terrorist.init_included()
+        Org.init_included()
+    # Call func every 6 hours if KFM_SYNC_TIMER is not set
+    threading.Timer(
+            os.getenv('KFM_SYNC_TIMER', 21600), call_kfn, (app,)
+        ).start()
+
+
+call_async_kfm(app)
