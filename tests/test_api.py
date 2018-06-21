5753799d327426c164f68ac0a84dbfc2f6413d42
Berezovskiy Denis <berezovskiy.denis@gmail.com>
2018-06-21 10:49:37 +0600

    Add methods to test API

--- tests/test_api.py
+++ tests/test_api.py
@@ -28,3 +28,23 @@ class FlaskClientTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 404)
         json_response = json.loads(response.get_data(as_text=True))
         self.assertEqual(json_response['error'].lower(), 'not found')
+
+    def test_400_terrorist(self):
+        response = self.client.get('/api/v1/terrorist')
+        self.assertEqual(response.status_code, 400)
+        json_response = json.loads(response.get_data(as_text=True))
+        self.assertEqual(json_response['error'].lower(), 'bad request')
+
+    def test_400_terrorist_name(self):
+        response = self.client.get('/api/v1/terrorist?lname=a')
+        self.assertEqual(response.status_code, 400)
+        json_response = json.loads(response.get_data(as_text=True))
+        self.assertEqual(json_response['error'].lower(), 'bad request')
+        self.assertIn('fname', json_response['message'].lower())
+
+    def test_400_org(self):
+        response = self.client.get('/api/v1/org')
+        self.assertEqual(response.status_code, 400)
+        json_response = json.loads(response.get_data(as_text=True))
+        self.assertEqual(json_response['error'].lower(), 'bad request')
+        self.assertIn('name', json_response['message'].lower())
